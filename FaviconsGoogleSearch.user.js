9859203d67425fcd3dfd71c9cf5b79499efccef9
Guillaume Lachance <glachance@cmaisonneuve.qc.ca>
2016-02-05 08:19:03 -0500

    Update FaviconsGoogleSearch.user.js

--- FaviconsGoogleSearch.user.js
+++ FaviconsGoogleSearch.user.js
@@ -5,6 +5,7 @@
 // @description  Faviconize google search
 // @author       glachance
 // @require      http://code.jquery.com/jquery-latest.js
+// @updateurl    https://github.com/glachancecmaisonneuve/UserScripts/raw/master/FaviconsGoogleSearch.user.js
 // @match        https://www.google.ca/*
 // @match        https://www.google.com/*
 // @include      https://www.google.ca/*
