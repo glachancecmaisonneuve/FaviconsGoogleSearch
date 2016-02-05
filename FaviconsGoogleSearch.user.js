0f87b7a2fd44ffb681521b931e0d6a1e0a282a21
glachancecmaisonneuve <glachance@cmaisonneuve.qc.ca>
2016-02-05 07:19:15 -0500

    initial

---
+++ FaviconsGoogleSearch.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Favicons in Google Search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Faviconize google search
+// @author       You
+// @require      http://code.jquery.com/jquery-latest.js
+// @match        https://www.google.ca/*
+// @match        https://www.google.com/*
+// @include      https://www.google.ca/*
+// @include      https://www.google.com/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+var favicon_src;
+
+favicon_src = function(url) {
+  return "https://www.google.com/s2/favicons?domain=" + url;
+};
+
+$(function() {
+  return $('h3.r > a').each(function() {
+    return $(this).prepend("<img style='position:absolute; overflow:hidden; width: 32px; left: -40px; top: 0px;' src=" + favicon_src($(this).attr("href") + " />"));
+  });
+});
