82bbf9d56f1904a629edf92773fc24400a03444c
glachancecmaisonneuve <glachance@cmaisonneuve.qc.ca>
2018-03-10 22:15:23 -0500

    v0.2

--- FaviconsGoogleSearch.user.js
+++ FaviconsGoogleSearch.user.js
@@ -5,7 +5,7 @@
 // @description  Faviconize google search
 // @author       glachancecmaisonneuve
 // @require      http://code.jquery.com/jquery-latest.js
-// @icon         https://github.com/glachancecmaisonneuve/UserScripts/raw/master/FaviconsGoogleSearch.png
+// @icon         https://github.com/glachancecmaisonneuve/UserScripts/raw/master/FaviconsGoogleSearchIcon.png
 // @updateurl    https://github.com/glachancecmaisonneuve/UserScripts/raw/master/FaviconsGoogleSearch.user.js
 // @match        https://www.google.ca/*
 // @match        https://www.google.com/*
@@ -17,11 +17,12 @@
 var favicon_src;
 
 favicon_src = function(url) {
-  return "https://www.google.com/s2/favicons?domain=" + url;
+    var domain = new URL(url).host;
+    return "https://www.google.com/s2/favicons?domain=" + domain;
 };
 
 $(function() {
   return $('h3.r > a').each(function() {
-    return $(this).prepend("<img style='position:absolute; overflow:hidden; width: 32px; left: -40px; top: 0px;' src=" + favicon_src($(this).attr("href") + " />"));
+    return $(this).prepend("<img style='position:absolute; overflow:hidden; width: 32px; left: -40px; top: 0px;' src=" + favicon_src($(this).attr("href")) + " />");
   });
 });
